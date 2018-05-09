da5e44c4d095f1a6538f1d664b87381c849e3cb2
rudifa <rudi.farkas@gmail.com>
2018-05-09 16:52:52 +0200

    LoginController: add inputsContainerView

--- my_gameofchats/Controller/LoginController.swift
+++ my_gameofchats/Controller/LoginController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +23,18 @@ class LoginController: UIViewController {
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151, alpha: 1)
         
         //UIApplication.shared.statusBarView?.backgroundColor = UIColor.red // just testing
+        
+        constrainInputsContainerView()
+        
+        view.addSubview(inputsContainerView)
+    }
+    
+    func constrainInputsContainerView() {
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:1.0, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150.0).isActive = true
+
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
