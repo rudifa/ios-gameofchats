c11b5d6b03a01de16e6c3d4717a7e3de22854d6a
rudifa <rudi.farkas@gmail.com>
2018-05-09 17:29:19 +0200

    LoginController: add loginRegisterButton

--- my_gameofchats/Controller/LoginController.swift
+++ my_gameofchats/Controller/LoginController.swift
@@ -18,23 +18,41 @@ class LoginController: UIViewController {
         view.layer.masksToBounds = true
         return view
     }()
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 100, g: 121, b: 181)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(r: 61, g: 91, b: 151, alpha: 1)
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         
         //UIApplication.shared.statusBarView?.backgroundColor = UIColor.red // just testing
         
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
         constrainInputsContainerView()
+        constrainLoginRegisterButton()
     }
     
     func constrainInputsContainerView() {
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:1.0, constant: -24).isActive = true
-        inputsContainerView.heightAnchor.constraint(equalToConstant: 150.0).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func constrainLoginRegisterButton() {
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor, multiplier: 1.0).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -43,8 +61,8 @@ class LoginController: UIViewController {
 }
 
 extension UIColor {
-    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, alpha: CGFloat) {
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: alpha)
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
 }
 
