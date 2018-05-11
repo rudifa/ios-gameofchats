f03210b4ac803c933741ff92f576e50179a6d1a8
rudifa <rudi.farkas@gmail.com>
2018-05-11 17:35:50 +0200

    add checkIfUserIsLoggedIn, display user name in MessageView navigationItem.title

--- my_gameofchats/Controller/MessageController.swift
+++ my_gameofchats/Controller/MessageController.swift
@@ -17,10 +17,27 @@ class MessageController: UITableViewController {
         // Not using the storyboard - create the accessories.
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
-        // user is not logged in
+//        checkIfUserIsLoggedIn()
+    }
+    
+    func checkIfUserIsLoggedIn() {
         if Auth.auth().currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        } else {
+            // get user info
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                print(snapshot)
+                if let dict = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dict["name"] as? String
+                }
+            }, withCancel: nil)
         }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        checkIfUserIsLoggedIn()
     }
 
     @objc func handleLogout() {
