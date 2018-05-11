a5b16b50dff2b26302ec7f4be74dd51e62ef07d4
rudifa <rudi.farkas@gmail.com>
2018-05-11 18:47:52 +0200

    NewMessageController : add table cells

--- my_gameofchats/Controller/NewMessageController.swift
+++ my_gameofchats/Controller/NewMessageController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewMessageController: UITableViewController {
 
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +20,15 @@ class NewMessageController: UITableViewController {
     
     @objc func handleCancel() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "hello"
+        return cell
     }
 }
